98de521aa27c1e84b13af632ddc75e4c4347950f
harikach521 <dimpulchintala@gmail.com>
2021-11-10 23:11:55 -0600

    updated the files

--- app.js
+++ app.js
@@ -4,50 +4,33 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//Part --3 (1)
-const connectionString= process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString, {useNewUrlPaper: true, useUnifiedTopology: true});
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var omletRouter = require('./routes/omlet');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
-var omlet=require("./models/omlet");
-
-//Part -3 (10)
-//we can seed the collection if needed on server start
-async function recreatDB(){
-  //Deleting everything
-  await omlet.deleteMany();
-
-  let instance1 = new 
-  omlet({omlet_type : "Bread Omlet", size:'large', cost:12});
-  instance1.save(function(err,doc){
-    if(err) return console.error(err);
-    console.log("First Object Saved")
-  });
-
-  let instance2 = new 
-  omlet({omlet_type : "French Omlet", size:'large', cost:9});
-  instance2.save(function(err,doc){
-    if(err) return console.error(err);
-    console.log("Second Object Saved")
-  });
-
-  let instance3 = new 
-  omlet({omlet_type : "Spinach Omlet", size:'large', cost:10});
-  instance3.save(function(err,doc){
-    if(err) return console.error(err);
-    console.log("Third Object Saved")
-  });
-}
-
-let reseed = true;
-if(reseed) { recreateDB();}
+var Costume = require("./models/costume"); 
+var bullet=require("./routes/bullet");
+var resource=require("./routes/resource");
 
 var app = express();
+
+
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")});
+ recreateDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -64,7 +47,8 @@ app.use('/users', usersRouter);
 app.use('/omlet', omletRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
-app.use('/omlet',omlet);
+app.use('/bullet',bullet);
+app.use('/resource',resource);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -83,12 +67,32 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+// We can seed the collection if needed on 
 
-
-//Part 3 (3)
-//Get the default connection
-var db=mongoose.connection;
-
-//Bind connection to error event
-db.on('error',console.error.bind(console, 'MongoDB Connection Error!'));
-db.once("open",function(){ console.log("Connection to DB Succeeded.")});
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany(); 
+ 
+  let instance1 = new 
+Costume({costume_type:"ghost",  size:'small', 
+cost:26.4});
+let instance2 = new 
+Costume({costume_type:"anf",  size:'large', 
+cost:87.4});
+let instance3 = new 
+Costume({costume_type:"nhg",  size:'medium', 
+cost:98.4}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+  instance2.save( function(err,doc) { 
+    if(err) return console.error(err); 
+    console.log("Second object saved") 
+}); 
+instance3.save( function(err,doc) { 
+  if(err) return console.error(err); 
+  console.log("Third object saved") 
+}); 
+} 
+ 
--- models/omlet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose") 
-const omletSchema = mongoose.Schema({ 
- omlet_type: String, 
- size: String, 
- cost: Number 
-}) 
- 
-module.exports = mongoose.model("omlet", 
-omletSchema)
