e10e135ac24393602081c534b7e07098e77177c0
harikach521 <dimpulchintala@gmail.com>
2021-11-29 17:07:52 -0600

    protecting endpoints

--- routes/bullet.js
+++ routes/bullet.js
@@ -1,6 +1,19 @@
 var express = require('express');
 const costume_controlers= require('../controllers/costume');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+
 /* GET costumes */
 router.get('/', costume_controlers.costume_view_all_Page );
 
@@ -10,10 +23,26 @@ router.get('/detail', costume_controlers.costume_view_one_Page);
 router.get('/create', costume_controlers.costume_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', costume_controlers.costume_update_Page); 
+router.get('/update',secured, costume_controlers.costume_update_Page); 
 
 /* GET create costume page */ 
 router.get('/delete', costume_controlers.costume_delete_Page); 
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //part-4
 /* GET detail costume page */ 
 //router.get('/detail', costume_controlers.costume_view_one_Page); 
@@ -36,5 +65,5 @@ router.get('/delete', costume_controlers.costume_delete_Page);
 //router.get('/costume/:id', costume_controlers.costume_delete);
 
 
-module.exports = router;
+
 
