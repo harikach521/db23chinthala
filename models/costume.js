3470924f5c746be8cb921213f1017238a3e47675
harikach521 <dimpulchintala@gmail.com>
2021-11-30 17:40:40 -0600

    added bonus and Part6

--- models/costume.js
+++ models/costume.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const costumeSchema = mongoose.Schema({ 
- costume_type: String, 
+ costume_type: {type:String, required:true}, 
  size: String, 
- cost: Number 
+ cost: { type: Number, min: 5, max: 100, default: 0 },
 }) 
  
 module.exports = mongoose.model("Costume", 
